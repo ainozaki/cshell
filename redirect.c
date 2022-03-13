e8016ae12bae9ee722bd2cf7131f9af7d0dcc68d
ainnooo <aiai10piano@gmail.com>
2022-03-13 17:27:33 +0900

    support redirect 2>, 2>&1, 1>

--- redirect.c
+++ redirect.c
@@ -27,8 +27,13 @@ void do_redirect(char* argv[]) {
   }
 
   /* > */
+  /* 1> */
   n = search_argv(argv, ">");
+  if (!n) {
+    n = search_argv(argv, "1>");
+  }
   if (n > 0) {
+    printf(">\n");
     /* file only */
     fd = open(argv[n + 1], O_WRONLY | O_CREAT, 0644);
     if (fd < 0) {
@@ -37,6 +42,42 @@ void do_redirect(char* argv[]) {
     }
     close(1);
     dup2(fd, 1);
+    close(fd);
+
+    delete_argv(argv, n, 2);
+  }
+
+  /* 2> */
+  n = search_argv(argv, "2>");
+  if (n > 0) {
+    printf("2>\n");
+    /* file only */
+    fd = open(argv[n + 1], O_WRONLY | O_CREAT, 0644);
+    if (fd < 0) {
+      perror(argv[n + 1]);
+      exit(1);
+    }
+    close(2);
+    dup2(fd, 2);
+    close(fd);
+
+    delete_argv(argv, n, 2);
+  }
+
+  /* 2>&1 */
+  n = search_argv(argv, "2>&1");
+  if (n > 0) {
+    printf("2>&1\n");
+    /* file only */
+    fd = open(argv[n + 1], O_WRONLY | O_CREAT, 0644);
+    if (fd < 0) {
+      perror(argv[n + 1]);
+      exit(1);
+    }
+    close(1);
+    close(2);
+    dup2(fd, 1);
+    dup2(fd, 2);
     close(fd);
 
     delete_argv(argv, n, 2);
